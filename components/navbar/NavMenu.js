4aa129a261f232edcd83751856a30e494218e41d
Dibas Dauliya <dibasdauliya@gmail.com>
2021-10-11 07:03:14 +0545

    edit nav links

--- components/navbar/NavMenu.js
+++ components/navbar/NavMenu.js
@@ -102,10 +102,8 @@ const NavMenuItem = ({ handleClick, children, ...props }) => {
         mx={{ lg: '1.5em' }}
       >
         <NextLink
-          href={`/#${children.toLowerCase()}`}
-          // href={children === 'About' ? '/about' : `/#${children.toLowerCase()}`}
-          to={`/#${children.toLowerCase()}`}
-          // to={children === 'About' ? '/about' : `/#${children.toLowerCase()}`}
+          href={children === 'About' ? '/about' : `/#${children.toLowerCase()}`}
+          to={children === 'About' ? '/about' : `/#${children.toLowerCase()}`}
           passHref={true}
         >
           <Link _hover={{ color: secondary, textDecoration: 'none' }}>
